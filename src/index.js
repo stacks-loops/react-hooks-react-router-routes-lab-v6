53f2de0646722034f3fe92890eb8ce4ce1e26d24
Parker <p.cronin.connolly@gmail.com>
2024-02-16 07:58:31 -0500

    saving

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 
+const BrowserRouter = createBrowserRouter()
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+
+
+root.render(
+    <BrowserRouter>
+    <RouterProvider routes={routes} />
+    </BrowserRouter>
+);
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function ErrorPage() {
+
+
+  return (
+    <>
+      <header>
+      </header>
+      <main>
+      </main>
+    </>
+  );
+};
+
+export default ErrorPage;
